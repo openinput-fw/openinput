7f87c19979b5195efefb3dd6c8c21d8305fef847
Rafael Silva <silvagracarafael@gmail.com>
2020-03-25 20:33:26 +0000

    steelseries-crc: typo

--- tools/steelseries-crc.py
+++ tools/steelseries-crc.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
                         help='Shows debugging outputs.')
     parser.add_argument('-a', '--append',
                         action='store_true',
-                        help='Appends the crc result to the file (by default outputs only the crc, this prepends the frimware to the output).')
+                        help='Appends the crc result to the file (by default outputs only the crc, this prepends the firmware to the output).')
     parser.add_argument('-o', '--output', type=str,
                         help='Output file.')
     parser.add_argument('firmware', metavar='firmware.bin', type=str,
