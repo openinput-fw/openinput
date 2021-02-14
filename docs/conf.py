602fc788932c43c410512001651d7d751d666e55
Filipe Laíns <lains@riseup.net>
2021-02-14 16:42:19 +0000

    docs: show TODOs
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- docs/conf.py
+++ docs/conf.py
@@ -55,6 +55,8 @@ html_theme_options = {
     'sidebar_hide_name': True,
 }
 
+todo_include_todos = True
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named 'default.css' will overwrite the builtin 'default.css'.
