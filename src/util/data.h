cc414697d350d365f4647f1d1f71c67a7a84bdb6
Rafael Silva <silvagracarafael@gmail.com>
2021-02-22 17:06:14 +0000

    util: add bit macro
    Signed-off-by: Rafael Silva <silvagracarafael@gmail.com>

--- src/util/data.h
+++ src/util/data.h
@@ -8,4 +8,6 @@
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 #define max(a, b) (((a) > (b)) ? (a) : (b))
 
+#define BIT(x) (1 << (x))
+
 #define REG_SET(reg, mask, value) (reg = (reg & ~mask) | value)
