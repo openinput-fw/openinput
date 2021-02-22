65bf8db4d2d7f73c6f72cc93d170f1498ecd2ae2
Rafael Silva <silvagracarafael@gmail.com>
2021-02-22 21:22:19 +0000

    stm32f1: flash latency configuration
    Signed-off-by: Rafael Silva <silvagracarafael@gmail.com>

---
+++ src/platform/stm32f1/flash.h
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: 2021 Rafael Silva <silvagracarafael@gmail.com>
+ */
+
+#pragma once
+
+#include "util/types.h"
+
+void flash_latency_config(u32 clock);
--- src/targets/stm32f1-generic/main.c
+++ src/targets/stm32f1-generic/main.c
@@ -5,9 +5,13 @@
 
 #include <stm32f1xx.h>
 
+#include "platform/stm32f1/flash.h"
 #include "platform/stm32f1/rcc.h"
 
 int main()
 {
+	flash_latency_config(72000000);
+
 	rcc_init(EXTERNAL_CLOCK_VALUE);
+	rcc_update_clock_tree();
 }
