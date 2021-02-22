71d6acd42c1c9d1663d07c9d9573a64aa6a06510
Rafael Silva <silvagracarafael@gmail.com>
2021-02-22 22:22:45 +0000

    stm32f1: atomic block
    Signed-off-by: Rafael Silva <silvagracarafael@gmail.com>

---
+++ src/platform/stm32f1/atomic.h
@@ -0,0 +1,50 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: 2021 Rafael Silva <silvagracarafael@gmail.com>
+ */
+
+#pragma once
+
+#include <stm32f1xx.h>
+
+#include "util/types.h"
+
+static inline uint32_t __iEnableIRQRetVal()
+{
+	__asm__ volatile("cpsie i" ::: "memory");
+
+	return 1;
+}
+
+static inline uint32_t __iDisableIRQRetVal()
+{
+	__asm__ volatile("cpsid i" ::: "memory");
+
+	return 1;
+}
+
+static inline void __iEnableIRQParam(const u32 *__s)
+{
+	__asm__ volatile("cpsie i" ::: "memory");
+	(void) __s;
+}
+
+static inline void __iDisableIRQParam(const u32 *__s)
+{
+	__asm__ volatile("cpsid i" ::: "memory");
+	(void) __s;
+}
+
+static inline void __iRestore(const u32 *__s)
+{
+	if (!*__s)
+		__asm__ volatile("cpsie i" ::: "memory");
+}
+
+#define ATOMIC_BLOCK(type)    for (type, __ToDo = __iDisableIRQRetVal(); __ToDo; __ToDo = 0)
+#define NONATOMIC_BLOCK(type) for (type, __ToDo = __iEnableIRQRetVal(); __ToDo; __ToDo = 0)
+
+#define ATOMIC_RESTORESTATE    u32 primask_save __attribute__((__cleanup__(__iRestore))) = __get_PRIMASK()
+#define ATOMIC_FORCEON	       u32 primask_save __attribute__((__cleanup__(__iEnableIRQParam))) = 0
+#define NONATOMIC_RESTORESTATE u32 primask_save __attribute__((__cleanup__(__iRestore))) = __get_PRIMASK()
+#define NONATOMIC_FORCEOFF     u32 primask_save __attribute__((__cleanup__(__iDisableIRQParam))) = 0
