eaf3e466ed8524a0e6a83fb9cf137c33f773ccb2
Rafael Silva <silvagracarafael@gmail.com>
2021-02-26 18:27:05 +0000

    stm32f1: usb: add usb configs
    Signed-off-by: Rafael Silva <silvagracarafael@gmail.com>

--- src/targets/stm32f1-generic/config/rival310.h
+++ src/targets/stm32f1-generic/config/rival310.h
@@ -1,10 +1,28 @@
 /*
  * SPDX-License-Identifier: MIT
  * SPDX-FileCopyrightText: 2021 Filipe Laíns <lains@riseup.net>
+ * SPDX-FileCopyrightText: 2021 Rafael Silva <silvagracarafael@gmail.com>
  */
 
-#include "platform/stm32f1/rcc.h"
+/* General Config */
 
 #define STM32F103x6
 
+/* Clock Config */
+
 #define EXTERNAL_CLOCK_VALUE STM32F1_CLOCK_12MHZ
+
+/* USB Config */
+
+#define USB_VID 0x1D50 /* Attributed by openmoko */
+#define USB_PID 0x616A /* Attributed by openmoko */
+
+/*
+ * The USB standard requires a 1.5k pullup resistor on D+
+ * Bluepill boards are known to come with the wrong value resistor here (R10)
+ * it is usually populated with a 10k or a 4.7k resistor and should be replaced with a 1.5k
+ */
+#define USB_DP_PU_IO                           \
+	{                                      \
+		.port = GPIO_PORT_A, .pin = 15 \
+	}
