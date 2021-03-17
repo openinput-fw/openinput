fc39f34305b9297882571c894b119d276f37d5ed
Perigoso <silvagracarafael@gmail.com>
2021-03-17 15:56:21 +0000

    hal: stm32f1: implement ticks hal
    Signed-off-by: Perigoso <silvagracarafael@gmail.com>

---
+++ src/platform/stm32f1/hal/ticks.c
@@ -0,0 +1,27 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: 2021 Rafael Silva <silvagracarafael@gmail.com>
+ */
+
+#include "platform/stm32f1/hal/ticks.h"
+#include "platform/stm32f1/systick.h"
+
+void ticks_hal_delay_ms(u32 ticks)
+{
+	return delay_ms(ticks);
+}
+
+void ticks_hal_delay_us(u32 ticks)
+{
+	return delay_us(ticks);
+}
+
+struct ticks_hal_t ticks_hal_init()
+{
+	struct ticks_hal_t hal = {
+		.delay_ms = ticks_hal_delay_ms,
+		.delay_us = ticks_hal_delay_us,
+		.drv_data = NULL,
+	};
+	return hal;
+}
