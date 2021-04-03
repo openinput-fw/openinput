3c5cf20561d2371696e7a2211c3b8b02208890b7
Rafael Silva <silvagracarafael@gmail.com>
2021-04-03 16:33:05 +0100

    hal: stm32f1: spi: stop exporting internal hal funcions
    Signed-off-by: Rafael Silva <silvagracarafael@gmail.com>

--- src/platform/stm32f1/hal/spi.c
+++ src/platform/stm32f1/hal/spi.c
@@ -5,14 +5,10 @@
 
 #include "platform/stm32f1/hal/spi.h"
 
-struct spi_hal_t spi_hal_init(struct spi_device_t *drv_data)
+void spi_hal_select(struct spi_hal_t interface, u8 state)
 {
-	struct spi_hal_t hal = {
-		.transfer = spi_hal_transfer,
-		.select = spi_hal_select,
-		.drv_data = drv_data,
-	};
-	return hal;
+	struct spi_device_t *drv_data = interface.drv_data;
+	return spi_select(*drv_data, state);
 }
 
 u8 spi_hal_transfer(struct spi_hal_t interface, u8 data)
@@ -21,8 +17,12 @@ u8 spi_hal_transfer(struct spi_hal_t interface, u8 data)
 	return spi_transfer_byte(*drv_data, data);
 }
 
-void spi_hal_select(struct spi_hal_t interface, u8 state)
+struct spi_hal_t spi_hal_init(struct spi_device_t *drv_data)
 {
-	struct spi_device_t *drv_data = interface.drv_data;
-	return spi_select(*drv_data, state);
+	struct spi_hal_t hal = {
+		.transfer = spi_hal_transfer,
+		.select = spi_hal_select,
+		.drv_data = drv_data,
+	};
+	return hal;
 }
