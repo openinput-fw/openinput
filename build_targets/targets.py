41ee6f56a4595c0bfe6712fa499308098577fb38
perigoso <silvagracarafael@gmail.com>
2021-04-15 00:36:44 +0100

    targets: add efm32gg12-generic target
    Signed-off-by: perigoso <perigoso@riseup.net>

--- build_targets/targets.py
+++ build_targets/targets.py
@@ -88,3 +88,59 @@ class STM32F1GenericTarget(families.STM32F1Family):
             required=True,
         )
         # TODO: let users specify their own files?
+
+
+class EFM32GG12GenericTarget(families.EFM32GG12Family):
+    name = 'efm32gg12-generic'
+
+    def init(self, args: Dict[str, Any]) -> None:
+        if 'config' not in args:
+            raise BuildConfigurationError(
+                f'No configuration for {self.name}, '
+                'please specify one with --config'
+            )
+
+        self.config = args['config']
+        if self.config not in self.configs():
+            raise BuildConfigurationError(f'Unknown configuration for {self.name}: {self.config}')
+
+        self.linker_script = os.path.join(self.name, f'{self.config}.ld')
+
+        assert isinstance(self.dependencies, list)
+        for dependency in self.dependencies:
+            if isinstance(dependency, dependencies.TinyUSBDependency):
+                dependency.include_files += self.target_files(
+                    os.path.join('config', f'{self.config}.h'),
+                )
+
+    def source(self) -> List[str]:
+        return self.target_files(
+            'main.c',
+        )
+
+    def include_files(self) -> List[str]:
+        return self.target_files(
+            os.path.join('config', f'{self.config}.h'),
+        )
+
+    @classmethod
+    def configs(cls) -> List[str]:
+        config_path = os.path.join(abs_target(cls.name), 'config')
+        return [
+            config.removesuffix('.h')
+            for config in os.listdir(config_path)
+        ]
+
+    @classmethod
+    def parser_append_group(cls, parser: argparse.ArgumentParser) -> None:
+        group = parser.add_argument_group(f'{cls.name} options')
+        group.add_argument(
+            '--config',
+            '-c',
+            type=str,
+            metavar='CONFIG',
+            choices=cls.configs(),
+            help='device configuration name',
+            required=True,
+        )
+        # TODO: let users specify their own files?
---
+++ src/targets/efm32gg12-generic/main.c
@@ -0,0 +1,15 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: 2021 Rafael Silva <perigoso@riseup.net>
+ */
+
+#include <em_device.h>
+
+#include "util/data.h"
+#include "util/types.h"
+
+int main()
+{
+	for (;;) {
+	}
+}
