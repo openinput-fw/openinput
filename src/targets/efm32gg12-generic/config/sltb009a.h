77464c2da4f2e62b822f15ccedee2e66f7bd74ce
perigoso <silvagracarafael@gmail.com>
2021-04-15 00:37:31 +0100

    targets: efm32gg12-generic: add sltb009a config
    Signed-off-by: perigoso <perigoso@riseup.net>

---
+++ src/targets/efm32gg12-generic/config/sltb009a.h
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: 2021 Rafael Silva <perigoso@riseup.net>
+ */
+
+/* clang-format off */
+
+/* General Config */
+
+#define EFM32GG12B810F1024GM64
