4e8b06f8ca8616e482d22528228d149705d384f5
Rafael Silva <perigoso@riseup.net>
2021-04-15 22:29:24 +0100

    platform: efm32gg: implement usb
    Signed-off-by: Rafael Silva <perigoso@riseup.net>

--- src/targets/efm32gg12-generic/config/sltb009a.h
+++ src/targets/efm32gg12-generic/config/sltb009a.h
@@ -8,3 +8,21 @@
 /* General Config */
 
 #define EFM32GG12B810F1024GM64
+
+/* Clock Config */
+
+#define EXTERNAL_CLOCK_VALUE 50000000UL
+
+/* Energy Management Unit Config */
+#define DCDC_ENABLE
+#define DCDC_VOLTAGE    1800.f
+#define DCDC_ACTIVE_I   50.f
+#define DCDC_SLEEP_I    100.f
+#define DCDC_REVERSE_I  0
+
+#define REGULATOR_OUT   3300.f
+
+/* USB Config */
+
+#define USB_VID 0x1D50 /* Attributed by openmoko */
+#define USB_PID 0x616A /* Attributed by openmoko */
