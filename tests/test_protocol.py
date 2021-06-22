bc71fb19f23074ee0186efd1ec7f1b404ddbbdb7
Filipe Laíns <lains@riseup.net>
2021-06-23 00:27:42 +0100

    tests: add tests for INFO FW_INFO function
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -8,3 +8,35 @@ def test_dispatch(basic_device):
     basic_device.protocol_dispatch([0x21])  # invalid length long
 
     basic_device.hid_send.assert_not_called()
+
+
+def test_fw_info_vendor(basic_device):
+    basic_device.protocol_dispatch([0x20, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00])
+
+    basic_device.hid_send.assert_called_with(
+        [0x21, 0x00, 0x01] + list(b'openinput-git') + [0x00] * 16
+    )
+
+
+def test_fw_info_version(basic_device, fw_version):
+    basic_device.protocol_dispatch([0x20, 0x00, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00])
+
+    basic_device.hid_send.assert_called_with(
+        [0x21, 0x00, 0x01] + list(fw_version.encode('ascii')) + [0x00] * (29 - len(fw_version))
+    )
+
+
+def test_fw_info_device_name(basic_device):
+    basic_device.protocol_dispatch([0x20, 0x00, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00])
+
+    basic_device.hid_send.assert_called_with(
+        [0x21, 0x00, 0x01] + list(b'basic test device') + [0x00] * 12
+    )
+
+
+def test_fw_info_unsupported(basic_device):
+    basic_device.protocol_dispatch([0x20, 0x00, 0x01, 0xFF, 0x00, 0x00, 0x00, 0x00])
+
+    basic_device.hid_send.assert_called_with(
+        [0x21, 0xFF, 0x01, 0x00, 0x01] + [0x00] * 27
+    )
