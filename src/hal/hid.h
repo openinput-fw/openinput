4b12f741f746b55e610b378edf600060b7083c44
Filipe Laíns <lains@riseup.net>
2021-06-23 16:45:05 +0100

    hal: hid: add documentation
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- src/hal/hid.h
+++ src/hal/hid.h
@@ -8,6 +8,8 @@
 #include "util/types.h"
 
 struct hid_hal_t {
+	/* send packet to the host */
 	int (*send)(struct hid_hal_t interface, u8 *buffer, size_t buffer_size);
+	/* arbitrary user data */
 	void *drv_data;
 };
