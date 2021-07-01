15a08e6b856100e5e94e12d0bfb3d93345a148f4
perigoso <perigoso@riseup.net>
2021-07-02 00:33:31 +0100

    targets: add atmega32u4 generic target
    Signed-off-by: perigoso <perigoso@riseup.net>

---
+++ src/targets/atmega32u4-generic/config/sample.h
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: 2021 Rafael Silva <perigoso@riseup.net>
+ */
+
+/* clang-format off */
+
+/* Clock Config */
+
+#define F_CPU 16000000UL
---
+++ src/targets/atmega32u4-generic/main.c
@@ -0,0 +1,15 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: 2021 Rafael Silva <perigoso@riseup.net>
+ */
+
+#include <avr/io.h>
+
+#include "util/data.h"
+#include "util/types.h"
+
+void main()
+{
+	for (;;) {
+	}
+}
