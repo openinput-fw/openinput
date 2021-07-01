6c01b657935fab7077727b62b3c2a756965e5b32
perigoso <perigoso@riseup.net>
2021-07-01 01:16:50 +0100

    targets: add sams70-generic target, add big-sammy config
    Signed-off-by: perigoso <perigoso@riseup.net>

---
+++ src/targets/sams70-generic/config/big-sammy.h
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: 2021 Rafael Silva <perigoso@riseup.net>
+ */
+
+/* clang-format off */
+
+/* General Config */
+
+#define __SAMS70J20B__
