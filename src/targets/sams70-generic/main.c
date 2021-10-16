56b64ec0685adb29ad3c8f5fe419436e703a9ce2
Rafael Silva <perigoso@riseup.net>
2021-10-16 01:24:07 +0100

    target/sams70-generic: add inits
    Signed-off-by: Rafael Silva <perigoso@riseup.net>

--- src/targets/sams70-generic/main.c
+++ src/targets/sams70-generic/main.c
@@ -6,11 +6,19 @@
 #include "util/data.h"
 #include "util/types.h"
 
+#include "platform/samx7x/cache.h"
+#include "platform/samx7x/eefc.h"
 #include "platform/samx7x/pmc.h"
 #include "platform/samx7x/systick.h"
+#include "platform/samx7x/wdt.h"
 
 void main()
 {
+	icache_disable();
+	dcache_disable();
+
+	wdt_disable();
+
 	pmc_init(12000000UL, 0UL);
 	pmc_init_usb();
 	pmc_update_clock_tree();
@@ -18,5 +26,8 @@ void main()
 	systick_init();
 
 	for (;;) {
+		delay_ms(500);
+		// TODO: blinky
+		delay_ms(500);
 	}
 }
