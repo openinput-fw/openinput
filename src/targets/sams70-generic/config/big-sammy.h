3dce8be30538c1cd6cb2290e5e61231c96baa07f
Rafael Silva <perigoso@riseup.net>
2021-11-05 03:13:31 +0000

    target/sams70-generic: implement basic mouse functionality
    Signed-off-by: Rafael Silva <perigoso@riseup.net>

--- src/targets/sams70-generic/config/big-sammy.h
+++ src/targets/sams70-generic/config/big-sammy.h
@@ -12,3 +12,21 @@
 /* Clock Config */
 
 #define EXTERNAL_CLOCK_VALUE 12000000UL
+
+/* Sensor Config */
+#define SENSOR_ENABLED
+#define SENSOR_DRIVER               PIXART_PMW
+
+#define SENSOR_FIRMWARE_BLOB        pmw3360_blob
+
+#define SENSOR_MOTION_IO            { .port = PIO_PORT_B, .pin = 3 }
+
+#define SENSOR_INTERFACE_SPEED      1000000
+
+/* o -> active low, 1 -> active high */
+#define SENSOR_INTERFACE_CS_POL     0
+#define SENSOR_INTERFACE_CS_IO      { .port = PIO_PORT_A, .pin = 10 }
+
+#define SENSOR_INTERFACE_MISO_IO    { .port = PIO_PORT_A, .pin = 12 }
+#define SENSOR_INTERFACE_MOSI_IO    { .port = PIO_PORT_A, .pin = 13 }
+#define SENSOR_INTERFACE_SCK_IO     { .port = PIO_PORT_A, .pin = 14 }
